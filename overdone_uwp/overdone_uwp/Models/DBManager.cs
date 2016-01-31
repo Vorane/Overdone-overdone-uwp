79e723c65c8abb0365886004e9c1c95db41aa95c
Evans <munene.evansk@gmail.com>
2016-01-31 20:43:44 +0300

    Added the DBManager Class

---
+++ overdone_uwp/overdone_uwp/Models/DBManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace overdone_uwp.Models
+{
+    class DBManager
+    {
+    }
+}
