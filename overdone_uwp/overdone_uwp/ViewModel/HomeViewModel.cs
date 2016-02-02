c1458f4b2d8a9b202bde0075c62faabacc403170
Brian Savatia Keyonzo <briankeyonzo@yahoo.com>
2016-02-02 12:26:02 +0300

    Added images, updated homeviewmodel and styles

--- overdone_uwp/overdone_uwp/ViewModel/HomeViewModel.cs
+++ overdone_uwp/overdone_uwp/ViewModel/HomeViewModel.cs
@@ -4,15 +4,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using overdone_uwp.Views;
+using Windows.UI.Xaml.Controls;
+using overdone_uwp.Models;
+using System.Collections.ObjectModel;
 
 namespace overdone_uwp.ViewModel
 {
     //class: controls the home view
     class HomeViewModel : BaseViewModel<Home>
     {
+        public Grid _calendar_grid;
+        double _calendar_panel_height;
+        public double original_height;
+        public CalendarView calendar;
+        private DateTimeOffset _current_date;
+        private ObservableCollection<Models.task> _taskList;
+
         public HomeViewModel(Home view) : base(view)
         {
+            TestModel();
             _view.DataContext = this;       
+        }
+
+        // function: testing the model, no functional use
+        private void TestModel()
+        {
+            _taskList = new ObservableCollection<task>();
+
+            for(int i = 0; i < 15; i++)
+            {
+                task _task = new task();
+                _task.task_details = i + " testing the model.";
+                _task.task_name = i + " test";
+                _taskList.Add(_task);
+            }
+        }
+
+        public ObservableCollection<Models.task> TaskList
+        {
+            get
+            {
+                return _taskList;
+            }
+        }
+
+        public DateTimeOffset CurrentDate
+        {
+            get
+            {
+                return _current_date;
+            }
+            set
+            {
+                _current_date = value;
+            }
+        }
+
+        public void SetCalendar(CalendarView _calendar)
+        {
+            calendar = _calendar;
+        }
+
+        public  string Header
+        {
+            get
+            {
+                return "Home";
+            }
+        }
+        public double CalendarPanelHeight
+        {
+            get
+            {
+
+                return _calendar_panel_height;
+            }
+            set
+            {
+                _calendar_panel_height = value;
+                if (_calendar_grid != null)
+                    _calendar_grid.Height = value;
+              
+            }
+        }
+
+
+        /* Calendar Height control stuff */
+
+        public void ChangeCalendarHeight()
+        {
+
+            // Uncomment if using 2 weeks in view instead, has animations and much simpler logic
+            /*
+            if(calendar.NumberOfWeeksInView == 2)
+            {
+               calendar.NumberOfWeeksInView = 6;
+            }
+            else
+            {
+                calendar.NumberOfWeeksInView = 2;
+                calendar.SetDisplayDate(_current_date);
+            }
+            */
+
+            // Use if using 1 week in view, no animations
+            if (_calendar_grid.Height == original_height)
+            {
+                _calendar_grid.Height = original_height / 6;
+                calendar.NumberOfWeeksInView = 2;
+
+                calendar.SetDisplayDate(_current_date);
+            }
+            else
+            {
+                _calendar_grid.Height = original_height;
+                calendar.NumberOfWeeksInView = 6;
+                calendar.SetDisplayDate(_current_date);
+            }
+
+
         }
     }
 }
--- overdone_uwp/overdone_uwp/Views/Home.xaml.cs
+++ overdone_uwp/overdone_uwp/Views/Home.xaml.cs
@@ -2,6 +2,10 @@
 using Windows.UI.Xaml.Controls;
 using overdone_uwp.ViewModel;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml;
+using System;
+using Windows.UI.Xaml.Input;
+using System.Linq;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,7 +16,8 @@ namespace overdone_uwp.Views
     /// </summary>
     public sealed partial class Home : Page
     {
-        HomeViewModel _viewModel;
+        HomeViewModel _presenter;
+        Grid _openContext;
         public Home()
         {
             this.InitializeComponent();
@@ -22,7 +27,79 @@ namespace overdone_uwp.Views
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             var parameter = e.Parameter as ApplicationViewModel;
-            _viewModel = new HomeViewModel(this);
+            _presenter = new HomeViewModel(this);
+        }
+
+        // calendar logic
+
+        private void ExpandCalendar(object sender, RoutedEventArgs e)
+        {
+            _presenter.ChangeCalendarHeight();
+
+        }
+
+        private void MonthViewScrollViewer_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            var calendar_scroll_viewer = (ScrollViewer)sender;
+
+            var height = calendar_scroll_viewer.ActualHeight;
+
+            _presenter.CalendarPanelHeight = height;
+            _presenter.original_height = height;
+
+            // Uncomment if using 2 weeks in view
+
+            /*
+            _presenter.ChangeCalendarHeight();\
+            */
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            _presenter._calendar_grid = (Grid)sender;
+
+            _presenter.ChangeCalendarHeight();
+            _presenter.ChangeCalendarHeight();
+        }
+
+        private void FlowCalendar_Loaded(object sender, RoutedEventArgs e)
+        {
+            _presenter.SetCalendar((CalendarView)sender);
+            ((CalendarView)sender).SetDisplayDate(_presenter.CurrentDate = DateTime.Now);
+        }
+
+        private void FlowCalendar_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            _presenter.CurrentDate = sender.SelectedDates[0];
+            sender.SetDisplayDate(_presenter.CurrentDate);
+        }
+
+
+
+        private void TaskItemParent_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Grid parent = (Grid)sender;
+            var children = parent.Children;
+        
+            Grid contextMenu = (Grid)children.ElementAt(1);
+            _openContext = contextMenu;
+            if (contextMenu.Height != 0)
+            {
+                contextMenu.Height = 0;
+            }
+            else
+            {
+                contextMenu.Height = Double.NaN;
+            }
+        }
+
+        private void TaskListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_openContext != null)
+            {
+                _openContext.Height = 0;
+            }
         }
 
     }
