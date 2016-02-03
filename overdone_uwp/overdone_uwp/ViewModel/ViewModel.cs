83b60ad1d49a90c2e64a71dbc850d764dbb527a4
Evans <munene.evansk@gmail.com>
2016-02-03 10:55:29 +0300

    added lists and task methods to view model

--- overdone_uwp/overdone_uwp/ViewModel/ViewModel.cs
+++ overdone_uwp/overdone_uwp/ViewModel/ViewModel.cs
@@ -12,7 +12,9 @@ namespace overdone_uwp.ViewModel
     class ViewModel : INotifyPropertyChanged 
     {
         ObservableCollection<task> AllTasks { get; set; }
-
+        ObservableCollection<task> FolderTasks { get; set; }
+        ObservableCollection<folder> AllFolders { get; set; }
+        DBManager DB = new DBManager();
 
         public ViewModel()
         {
@@ -25,22 +27,59 @@ namespace overdone_uwp.ViewModel
 
 
         #region task List managers
-        private void AddTask(task NewTask)
+        //function: create a new task
+        public void AddTask(task NewTask)
         {
             try
             {
+                NewTask.PropertyChanged += TaskPropertyChanged;
                 AllTasks.Add(NewTask);
                 NotifyPropertyChanged("AllTasks");
+                DB.AddTask(NewTask);
+            }
+            catch { }
+        }
+        //function: update a task
+        public void UpdateTask(task SelectedTask)
+        {
+            try
+            {
+                
             }
             catch { }
         }
         #endregion
 
         #region Folder Managers
+        public void AddFolder(folder NewFolder)
+        {
+            try
+            {
+                AllFolders.Add(NewFolder);
+                NotifyPropertyChanged("AllFolders");
+                DB.AddFolder(NewFolder);
+            }
+            catch { }
+        }
         #endregion
 
         #region List Managers
-
+        //function: initialize items of list and property listener
+        public void InitializeAllLists()
+        {
+            try
+            {
+                AllFolders = DB.GetAllFolders();
+                AllTasks = DB.GetPendingTasks();
+                foreach (task t in AllTasks)
+                {
+                    t.PropertyChanged += TaskPropertyChanged;
+                }
+            }
+            catch
+            {
+            }
+        }
         #endregion
 
         #region Notify Event Managers
@@ -56,10 +95,19 @@ namespace overdone_uwp.ViewModel
             }
         }
         //function event handler when a task is modified
-
+        private void TaskPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            try
+            {
+                if (e.PropertyName == "task_status")
+                {                    
+                }
+            }
+            catch { }
+        }
         #endregion
 
 
-        
+
     }
 }
