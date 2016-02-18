7cbf89e559b0b7b76573b731d63d40d6e0835b39
Evans <munene.evansk@gmail.com>
2016-02-19 00:04:48 +0300

    Added Button Event Handlerss Removed DeleteRoutines Button

--- overdone_uwp/overdone_uwp/Views/Settings.xaml.cs
+++ overdone_uwp/overdone_uwp/Views/Settings.xaml.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using overdone_uwp.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,24 +25,68 @@ namespace overdone_uwp.Views
     /// </summary>
     public sealed partial class Settings : Page
     {
+        AppViewModel _viewmodel;
         public Settings()
         {
+            _viewmodel = AppViewModel.GetViewModel();
+            this.DataContext = _viewmodel;
             this.InitializeComponent();
+            SetUpPageAnimation();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (MainPage.RootFrame.CanGoBack)
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            else
+                SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+
+            SystemNavigationManager.GetForCurrentView().BackRequested += (s, a) =>
+            {
+                if (Frame.CanGoBack)
+                {
+                    Frame.GoBack();
+                    a.Handled = true;
+                }
+            };
+        }
+
+        protected void SetUpPageAnimation()
+        {
+            TransitionCollection collection = new TransitionCollection();
+            NavigationThemeTransition theme = new NavigationThemeTransition();
+
+            var info = new ContinuumNavigationTransitionInfo();
+
+            theme.DefaultNavigationTransitionInfo = info;
+            collection.Add(theme);
+            this.Transitions = collection;
         }
 
         private void DeleteAllTasksButton_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                _viewmodel.DeleteAllTasks();
+            }
+            catch (Exception)
+            {
 
-        }
-
-        private void DeleteAllRoutinesButton_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+                throw;
+            }
+        }        
 
         private void DeleteAllFoldersButton_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                _viewmodel.DeleteAllFolders();
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
         }
     }
 }
